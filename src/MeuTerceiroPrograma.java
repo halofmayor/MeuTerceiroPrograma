0a6a8f593caf85f078faa479e9bed10240250c35
halof <halofmayor@gmail.com>
2024-01-08 02:38:14 +0000

    Initial commit

---
+++ src/MeuTerceiroPrograma.java
@@ -0,0 +1,7 @@
+public class MeuTerceiroPrograma {
+    public static void main(String[] args) {
+        //2 tipos de erro
+        //erros de compilação (Quando o programa nao compreende o que foi escrito. ex: escrever doube e nao double
+        //erro de execução (Exception) x = 10 e y = 0 (x/y) => erro de execução
+    }
+}
